3a1ee6204a73ac11094c70c053e246a217e34373
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-16 01:24:15 +0530

    Styled User Order Details Page

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,5 +1,39 @@
+import { Row, Col } from "react-bootstrap"
+import Table from 'react-bootstrap/Table'
+import {Link} from 'react-router-dom'
+
 const UserOrdersPage = () => {
-    return <p>This is the UserOrdersPage</p>
+    return (
+        <Row className="m-5">
+            <Col md={12}>
+                <h1>My Orders</h1>
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>User</th>
+                    <th>Date</th>
+                    <th>Total</th>
+                    <th>Delivered</th>
+                    <th>Order Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map((item, idx) => (
+                        <tr key={idx}>
+                            <td>{idx+1}</td>
+                            <td>Aditya Dhanekula</td>
+                            <td>16-06-2023</td>
+                            <td>$234</td>
+                            <td><i className={item}></i></td>
+                            <td><Link to="/user/order-details">View Details</Link></td>
+                        </tr>
+                    ))}
+                </tbody>
+                </Table>
+            </Col>
+        </Row>
+    )
 }
 
 export default UserOrdersPage
