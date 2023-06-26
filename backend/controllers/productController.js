5124ea022ffa2d2969b9691f816f3ad5ded06509
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-26 18:15:32 +0530

    Implemented Pagination and product sorting

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,26 @@
+const recordsPerPage = require("../config/pagination")
 const Product = require("../models/ProductModel")
 
-const getProducts = (req, res) => {
-    res.send("Handling Product Routes, e.g search for products")
+const getProducts = async (req, res, next) => {
+    // res.send("Handling Product Routes, e.g search for products")
+    try {
+        const pageNum = Number(req.query.pageNum) || 1
+        const totalProducts = await Product.countDocuments({})
+        
+        //Sort by name, rating, price
+        let sort = {}
+        const sortOption = req.query.sort || ""
+        if(sortOption) {
+            let sortOpt = sortOption.split("_")
+            sort = { [sortOpt[0]]: Number(sortOpt[1]) }
+            //Square brackets are required for dynamic keys in javaScript
+            console.log(sort)
+        }
+
+        const products = await Product.find({}).skip(recordsPerPage * (pageNum-1)).sort(sort).limit(recordsPerPage)
+        res.json({products, pageNum, paginationLinksNumber: Math.ceil(totalProducts/recordsPerPage)})
+    } catch (error){
+        next(error)
+    } 
 }
 module.exports = getProducts
