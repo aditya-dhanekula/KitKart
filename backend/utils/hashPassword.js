8c351a83a6b095875ebb6241ab2e124cc6c565e4
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-27 02:22:19 +0530

    Finished API endpoint for user login

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,5 +1,9 @@
-const bcrypt = require("bcryptjs")
-const salt = bcrypt.genSaltSync(10)
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 
-const hashPassword = password => bcrypt.hashSync(password, salt)
-module.exports = { hashPassword }
+const hashPassword = (password) => bcrypt.hashSync(password, salt);
+
+const comparePasswords = (inputPassword, hasedPassword) =>
+  bcrypt.compareSync(inputPassword, hasedPassword);
+
+module.exports = { hashPassword, comparePasswords };
