8d755be71f10386775a3716c10359c7be6390516
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-27 00:42:12 +0530

    Fixed Bug in hashPassword.js

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,8 +1,5 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs")
 const salt = bcrypt.genSaltSync(10)
 
-const hashPassword = (password) => {
-    bcrypt.hashSync(password, salt)
-}
-
-module.exports = {hashPassword}
+const hashPassword = password => bcrypt.hashSync(password, salt)
+module.exports = { hashPassword }
