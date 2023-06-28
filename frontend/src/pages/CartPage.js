6aa9501d837db752274a21ffefab83e87cc0df47
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-29 03:57:59 +0530

    Temporary fixes for errors due to changes in cartItemComponent

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,38 +1,55 @@
-import {Row, Col, Container, Alert, ListGroup, ListGroupItem, Button} from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import CartItemComponent from '../components/CartItemComponent'
+import {
+  Row,
+  Col,
+  Container,
+  Alert,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import CartItemComponent from "../components/CartItemComponent";
 
 const CartPage = () => {
-    return (
-        <Container fluid>
-            <Row className='mt-4'>
-                <Col md={8}>
-                    <h1>Shopping Cart</h1>
-                    <ListGroup variant='flush'>
-                        {Array.from({length: 3}).map((item, idx) => (
-                            <CartItemComponent key={idx}/>
-                        ))}
-                    </ListGroup>
-                <Alert variant="info">Your cart is empty</Alert>
-                </Col>
-                <Col md={4}>
-                    <ListGroup>
-                        <ListGroupItem>
-                            <h3>Subtotal (2 Items)</h3>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Price: <span className='fw-bold'>$892</span>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <LinkContainer to="/user/cart-details">
-                                <Button type='button'>Proceed To Checkout</Button>
-                            </LinkContainer>
-                        </ListGroupItem>
-                    </ListGroup>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent
+                item={{
+                  image: { path: "/images/tablets-category.png" },
+                  name: "Product name",
+                  price: 10,
+                  count: 10,
+                  quantity: 5,
+                }}
+                key={idx}
+              />
+            ))}
+          </ListGroup>
+          <Alert variant="info">Your cart is empty</Alert>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroupItem>
+              <h3>Subtotal (2 Items)</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              Price: <span className="fw-bold">$892</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              <LinkContainer to="/user/cart-details">
+                <Button type="button">Proceed To Checkout</Button>
+              </LinkContainer>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default CartPage
+export default CartPage;
--- frontend/src/pages/user/UserCartDetailsPage.js
+++ frontend/src/pages/user/UserCartDetailsPage.js
@@ -1,79 +1,99 @@
-import { Container, Row, Col, Form, Alert, ListGroup, ListGroupItem, Button } from "react-bootstrap"
-import CartItemComponent from '../../components/CartItemComponent'
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Alert,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap";
+import CartItemComponent from "../../components/CartItemComponent";
 
 const UserCartDetailsPage = () => {
-    return (
-        <Container fluid>
-            <Row className="mt-4">
-                <h1>Cart Details</h1>
-                <Col md={8}>
-                    <br/>
-                    <Row>
-                        <Col md={6}>
-                            <h2>Shipping</h2>
-                            <b>Name</b>: Aditya Dhanekula <br/>
-                            <b>Address</b>: 8739 Mayflower St. Los Angeles, CA 90063<br/>
-                            <b>Phone</b>: 2972328192
-                        </Col>
-                        <Col md={6}>
-                        <h2>Payment Method</h2>
-                        <Form.Select>
-                            <option value="pp">
-                                PayPal
-                            </option>
-                            <option value="cod">
-                                Cash on Delivery (Delivery may be delayed)
-                            </option>
-                        </Form.Select>
-                        </Col>
-                        <Row>
-                            <Col>
-                                <Alert className="mt-3" variant="danger">
-                                    Not Delivered. In order to place an order, complete filling your profile with accurate details.
-                                </Alert>
-                            </Col>
-                            <Col>
-                                <Alert className="mt-3" variant="success">
-                                    Not paid yet
-                                </Alert>
-                            </Col>
-                        </Row>
-                    </Row>
-                    <br/> 
-                    <h2>Cart Items</h2>
-                    <ListGroup variant="flush">
-                        {Array.from({length: 3}).map((item, idx)=>(
-                            <CartItemComponent key={idx}></CartItemComponent>
-                        ))}
-                    </ListGroup>
-                </Col>
-                <Col md={4}>
-                    <ListGroup>
-                        <ListGroupItem>
-                            <h3>Order Summary</h3>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Items Price (After tax): <span className="fw-bold">$62</span>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Shipping: <span className="fw-bold">Included</span>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Tax: <span className="fw-bold">Included</span>
-                        </ListGroupItem>
-                        <ListGroupItem className="text-danger">
-                            Total Price (After tax): <span className="fw-bold">$62</span>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <div className="d-grid gap-2">
-                                <Button size="lg" variant="danger" type="button">Pay Now</Button>
-                            </div>
-                        </ListGroupItem>
-                    </ListGroup>
-                </Col>
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <h1>Cart Details</h1>
+        <Col md={8}>
+          <br />
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+              <b>Name</b>: Aditya Dhanekula <br />
+              <b>Address</b>: 8739 Mayflower St. Los Angeles, CA 90063
+              <br />
+              <b>Phone</b>: 2972328192
+            </Col>
+            <Col md={6}>
+              <h2>Payment Method</h2>
+              <Form.Select>
+                <option value="pp">PayPal</option>
+                <option value="cod">
+                  Cash on Delivery (Delivery may be delayed)
+                </option>
+              </Form.Select>
+            </Col>
+            <Row>
+              <Col>
+                <Alert className="mt-3" variant="danger">
+                  Not Delivered. In order to place an order, complete filling
+                  your profile with accurate details.
+                </Alert>
+              </Col>
+              <Col>
+                <Alert className="mt-3" variant="success">
+                  Not paid yet
+                </Alert>
+              </Col>
             </Row>
-        </Container>
-    )
-}
+          </Row>
+          <br />
+          <h2>Cart Items</h2>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent
+                item={{
+                  image: { path: "/images/tablets-category.png" },
+                  name: "Product name",
+                  price: 10,
+                  count: 10,
+                  quantity: 5,
+                }}
+                key={idx}
+              ></CartItemComponent>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroupItem>
+              <h3>Order Summary</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              Items Price (After tax): <span className="fw-bold">$62</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Shipping: <span className="fw-bold">Included</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              Tax: <span className="fw-bold">Included</span>
+            </ListGroupItem>
+            <ListGroupItem className="text-danger">
+              Total Price (After tax): <span className="fw-bold">$62</span>
+            </ListGroupItem>
+            <ListGroupItem>
+              <div className="d-grid gap-2">
+                <Button size="lg" variant="danger" type="button">
+                  Pay Now
+                </Button>
+              </div>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default UserCartDetailsPage
+export default UserCartDetailsPage;
