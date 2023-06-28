7af54d9d82ae185ebc38aeeab251226da422ddb0
Aditya Dhanekula <dhanekulaaditya@gmail.com>
2023-06-28 20:47:13 +0530

    Dynamic users on admin users page

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -12,10 +12,10 @@ import AttributesFilterComponent from '../components/filterQueryResultOptions/At
 import ProductForListComponent from '../components/ProductForListComponent';
 import PaginationComponent from '../components/PaginationComponent';
 
-import axios from "axios";
+//import axios from "axios";
 
 const ProductListPage = () => {
-    axios.get("/api/products").then((res) => console.log(res))
+    //axios.get("/api/products").then((res) => console.log(res))
     return (
         <Container fluid>
             <Row>
